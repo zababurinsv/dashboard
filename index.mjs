0636461fbecd62ba1dbcc7952ab3bbf86cafefdb
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-01-22 09:45:46 +0300

    init

--- index.mjs
+++ index.mjs
@@ -8,7 +8,6 @@ import compression from "compression";
 import formidableMiddleware from "express-formidable";
 import {promisify} from "util";
 import dotenv from "dotenv"
-import template from './templates/default.mjs'
 dotenv.config()
 const highWaterMark =  2;
 let app = express();
@@ -19,10 +18,8 @@ const queue = new Enqueue({
     maxSize: 200,
     timeout: 30000
 });
-console.log('~~~~~~~~~test~~~~~~~~~~~~~~~~')
 app.use(queue.getMiddleware());
 let whitelist = ['https://web3-monopoly.web.app','http://localhost:8886','http://localhost:8887','http://localhost:8888','https://xart-3e938.firebaseapp.com','https://xart-3e938.web.app','https://universitykids.ru','https://vashi-faili.web.app','https://vashi-faili.web.app',  'https://www.universitykids.ru', 'https://tuning-fork.firebaseapp.com','http://localhost:8888', 'https://jainagul-tezekbaeva.firebaseapp.com','https://tezekbaeva.firebaseapp.com']
-const account = `/3N8n4Lc8BMsPPyVHJXTivQWs7ER61bB7wQn`
 let corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
@@ -34,12 +31,6 @@ let corsOptions = {
 }
 
 app.use(formidableMiddleware());
-app.options('/auth', cors(corsOptions))
-app.post('/auth', async (req, res) => {
-
-    console.log('~~~~~~~~~test~~~~~~~~~~~~~~~~', req)
-    res.send(await template());
-})
 app.use( express.static('public'));
 app.use( express.static('static'));
 app.options('/*', cors(corsOptions))
